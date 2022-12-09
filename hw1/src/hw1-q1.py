9060641d4a7a70e30499dad41f30c24422681d37
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2022-12-09 23:22:17 +0000

    initial material commit

---
+++ hw1/src/hw1-q1.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+
+# Deep Learning Homework 1
+
+import argparse
+import random
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import utils
+
+
+def configure_seed(seed):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+class LinearModel(object):
+    def __init__(self, n_classes, n_features, **kwargs):
+        self.W = np.zeros((n_classes, n_features))
+
+    def update_weight(self, x_i, y_i, **kwargs):
+        raise NotImplementedError
+
+    def train_epoch(self, X, y, **kwargs):
+        for x_i, y_i in zip(X, y):
+            self.update_weight(x_i, y_i, **kwargs)
+
+    def predict(self, X):
+        """X (n_examples x n_features)"""
+        scores = np.dot(self.W, X.T)  # (n_classes x n_examples)
+        predicted_labels = scores.argmax(axis=0)  # (n_examples)
+        return predicted_labels
+
+    def evaluate(self, X, y):
+        """
+        X (n_examples x n_features):
+        y (n_examples): gold labels
+        """
+        y_hat = self.predict(X)
+        n_correct = (y == y_hat).sum()
+        n_possible = y.shape[0]
+        return n_correct / n_possible
+
+
+class Perceptron(LinearModel):
+    def update_weight(self, x_i, y_i, **kwargs):
+        """
+        x_i (n_features): a single training example
+        y_i (scalar): the gold label for that example
+        other arguments are ignored
+        """
+        # Q1.1a
+        raise NotImplementedError
+
+
+class LogisticRegression(LinearModel):
+    def update_weight(self, x_i, y_i, learning_rate=0.001):
+        """
+        x_i (n_features): a single training example
+        y_i: the gold label for that example
+        learning_rate (float): keep it at the default value for your plots
+        """
+        # Q1.1b
+        raise NotImplementedError
+
+
+class MLP(object):
+    # Q3.2b. This MLP skeleton code allows the MLP to be used in place of the
+    # linear models with no changes to the training loop or evaluation code
+    # in main().
+    def __init__(self, n_classes, n_features, hidden_size):
+        # Initialize an MLP with a single hidden layer.
+        raise NotImplementedError
+
+    def predict(self, X):
+        # Compute the forward pass of the network. At prediction time, there is
+        # no need to save the values of hidden nodes, whereas this is required
+        # at training time.
+        raise NotImplementedError
+
+    def evaluate(self, X, y):
+        """
+        X (n_examples x n_features)
+        y (n_examples): gold labels
+        """
+        # Identical to LinearModel.evaluate()
+        y_hat = self.predict(X)
+        n_correct = (y == y_hat).sum()
+        n_possible = y.shape[0]
+        return n_correct / n_possible
+
+    def train_epoch(self, X, y, learning_rate=0.001):
+        raise NotImplementedError
+
+
+def plot(epochs, valid_accs, test_accs):
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.xticks(epochs)
+    plt.plot(epochs, valid_accs, label='validation')
+    plt.plot(epochs, test_accs, label='test')
+    plt.legend()
+    plt.show()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model',
+                        choices=['perceptron', 'logistic_regression', 'mlp'],
+                        help="Which model should the script run?")
+    parser.add_argument('-epochs', default=20, type=int,
+                        help="""Number of epochs to train for. You should not
+                        need to change this value for your plots.""")
+    parser.add_argument('-hidden_size', type=int, default=200,
+                        help="""Number of units in hidden layers (needed only
+                        for MLP, not perceptron or logistic regression)""")
+    parser.add_argument('-layers', type=int, default=1,
+                        help="""Number of hidden layers (needed only for MLP,
+                        not perceptron or logistic regression)""")
+    parser.add_argument('-learning_rate', type=float, default=0.001,
+                        help="""Learning rate for parameter updates (needed for
+                        logistic regression and MLP, but not perceptron)""")
+    opt = parser.parse_args()
+
+    utils.configure_seed(seed=42)
+
+    add_bias = opt.model != "mlp"
+    data = utils.load_classification_data(bias=add_bias)
+    train_X, train_y = data["train"]
+    dev_X, dev_y = data["dev"]
+    test_X, test_y = data["test"]
+
+    n_classes = np.unique(train_y).size  # 10
+    n_feats = train_X.shape[1]
+
+    # initialize the model
+    if opt.model == 'perceptron':
+        model = Perceptron(n_classes, n_feats)
+    elif opt.model == 'logistic_regression':
+        model = LogisticRegression(n_classes, n_feats)
+    else:
+        model = MLP(n_classes, n_feats, opt.hidden_size, opt.layers)
+    epochs = np.arange(1, opt.epochs + 1)
+    valid_accs = []
+    test_accs = []
+    for i in epochs:
+        print('Training epoch {}'.format(i))
+        train_order = np.random.permutation(train_X.shape[0])
+        train_X = train_X[train_order]
+        train_y = train_y[train_order]
+        model.train_epoch(
+            train_X,
+            train_y,
+            learning_rate=opt.learning_rate
+        )
+        valid_accs.append(model.evaluate(dev_X, dev_y))
+        test_accs.append(model.evaluate(test_X, test_y))
+
+    # plot
+    plot(epochs, valid_accs, test_accs)
+
+
+if __name__ == '__main__':
+    main()
---
+++ hw1/src/hw1-q2.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+
+# Deep Learning Homework 1
+
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from matplotlib import pyplot as plt
+
+import utils
+
+
+# Q2.1
+class LogisticRegression(nn.Module):
+
+    def __init__(self, n_classes, n_features, **kwargs):
+        """
+        n_classes (int)
+        n_features (int)
+
+        The __init__ should be used to declare what kind of layers and other
+        parameters the module has. For example, a logistic regression module
+        has a weight matrix and bias vector. For an idea of how to use
+        pytorch to make weights and biases, have a look at
+        https://pytorch.org/docs/stable/nn.html
+        """
+        super().__init__()
+        # In a pytorch module, the declarations of layers needs to come after
+        # the super __init__ line, otherwise the magic doesn't work.
+
+    def forward(self, x, **kwargs):
+        """
+        x (batch_size x n_features): a batch of training examples
+
+        Every subclass of nn.Module needs to have a forward() method. forward()
+        describes how the module computes the forward pass. In a log-lineear
+        model like this, for example, forward() needs to compute the logits
+        y = Wx + b, and return y (you don't need to worry about taking the
+        softmax of y because nn.CrossEntropyLoss does that for you).
+
+        One nice thing about pytorch is that you only need to define the
+        forward pass -- this is enough for it to figure out how to do the
+        backward pass.
+        """
+        raise NotImplementedError
+
+
+# Q2.2
+class FeedforwardNetwork(nn.Module):
+    def __init__(
+            self, n_classes, n_features, hidden_size, layers,
+            activation_type, dropout, **kwargs):
+        """
+        n_classes (int)
+        n_features (int)
+        hidden_size (int)
+        layers (int)
+        activation_type (str)
+        dropout (float): dropout probability
+
+        As in logistic regression, the __init__ here defines a bunch of
+        attributes that each FeedforwardNetwork instance has. Note that nn
+        includes modules for several activation functions and dropout as well.
+        """
+        super().__init__()
+        # Implement me!
+
+    def forward(self, x, **kwargs):
+        """
+        x (batch_size x n_features): a batch of training examples
+
+        This method needs to perform all the computation needed to compute
+        the output logits from x. This will include using various hidden
+        layers, pointwise nonlinear functions, and dropout.
+        """
+        raise NotImplementedError
+
+
+def train_batch(X, y, model, optimizer, criterion, **kwargs):
+    """
+    X (n_examples x n_features)
+    y (n_examples): gold labels
+    model: a PyTorch defined model
+    optimizer: optimizer used in gradient step
+    criterion: loss function
+
+    To train a batch, the model needs to predict outputs for X, compute the
+    loss between these predictions and the "gold" labels y using the criterion,
+    and compute the gradient of the loss with respect to the model parameters.
+
+    Check out https://pytorch.org/docs/stable/optim.html for examples of how
+    to use an optimizer object to update the parameters.
+
+    This function should return the loss (tip: call loss.item()) to get the
+    loss as a numerical value that is not part of the computation graph.
+    """
+    raise NotImplementedError
+
+
+def predict(model, X):
+    """X (n_examples x n_features)"""
+    scores = model(X)  # (n_examples x n_classes)
+    predicted_labels = scores.argmax(dim=-1)  # (n_examples)
+    return predicted_labels
+
+
+def evaluate(model, X, y):
+    """
+    X (n_examples x n_features)
+    y (n_examples): gold labels
+    """
+    model.eval()
+    y_hat = predict(model, X)
+    n_correct = (y == y_hat).sum().item()
+    n_possible = float(y.shape[0])
+    model.train()
+    return n_correct / n_possible
+
+
+def plot(epochs, plottable, ylabel='', name=''):
+    plt.clf()
+    plt.xlabel('Epoch')
+    plt.ylabel(ylabel)
+    plt.plot(epochs, plottable)
+    plt.savefig('%s.pdf' % (name), bbox_inches='tight')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model',
+                        choices=['logistic_regression', 'mlp'],
+                        help="Which model should the script run?")
+    parser.add_argument('-epochs', default=20, type=int,
+                        help="""Number of epochs to train for. You should not
+                        need to change this value for your plots.""")
+    parser.add_argument('-batch_size', default=1, type=int,
+                        help="Size of training batch.")
+    parser.add_argument('-learning_rate', type=float, default=0.01)
+    parser.add_argument('-l2_decay', type=float, default=0)
+    parser.add_argument('-hidden_sizes', type=int, default=100)
+    parser.add_argument('-layers', type=int, default=1)
+    parser.add_argument('-dropout', type=float, default=0.3)
+    parser.add_argument('-activation',
+                        choices=['tanh', 'relu'], default='relu')
+    parser.add_argument('-optimizer',
+                        choices=['sgd', 'adam'], default='sgd')
+    opt = parser.parse_args()
+
+    utils.configure_seed(seed=42)
+
+    data = utils.load_classification_data()
+    dataset = utils.ClassificationDataset(data)
+    train_dataloader = DataLoader(
+        dataset, batch_size=opt.batch_size, shuffle=True)
+
+    dev_X, dev_y = dataset.dev_X, dataset.dev_y
+    test_X, test_y = dataset.test_X, dataset.test_y
+
+    n_classes = torch.unique(dataset.y).shape[0]  # 10
+    n_feats = dataset.X.shape[1]
+
+    # initialize the model
+    if opt.model == 'logistic_regression':
+        model = LogisticRegression(n_classes, n_feats)
+    else:
+        model = FeedforwardNetwork(
+            n_classes,
+            n_feats,
+            opt.hidden_size,
+            opt.layers,
+            opt.activation,
+            opt.dropout
+        )
+
+    # get an optimizer
+    optims = {"adam": torch.optim.Adam, "sgd": torch.optim.SGD}
+
+    optim_cls = optims[opt.optimizer]
+    optimizer = optim_cls(
+        model.parameters(),
+        lr=opt.learning_rate,
+        weight_decay=opt.l2_decay)
+
+    # get a loss criterion
+    criterion = nn.CrossEntropyLoss()
+
+    # training loop
+    epochs = torch.arange(1, opt.epochs + 1)
+    train_mean_losses = []
+    valid_accs = []
+    train_losses = []
+    for ii in epochs:
+        print('Training epoch {}'.format(ii))
+        for X_batch, y_batch in train_dataloader:
+            loss = train_batch(
+                X_batch, y_batch, model, optimizer, criterion)
+            train_losses.append(loss)
+
+        mean_loss = torch.tensor(train_losses).mean().item()
+        print('Training loss: %.4f' % (mean_loss))
+
+        train_mean_losses.append(mean_loss)
+        valid_accs.append(evaluate(model, dev_X, dev_y))
+        print('Valid acc: %.4f' % (valid_accs[-1]))
+
+    print('Final Test acc: %.4f' % (evaluate(model, test_X, test_y)))
+    # plot
+    if opt.model == "logistic_regression":
+        config = "{}-{}".format(opt.learning_rate, opt.optimizer)
+    else:
+        config = "{}-{}-{}-{}-{}-{}-{}".format(opt.learning_rate, opt.hidden_size, opt.layers, opt.dropout, opt.activation, opt.optimizer, opt.batch_size)
+
+    plot(epochs, train_mean_losses, ylabel='Loss', name='{}-training-loss-{}'.format(opt.model, config))
+    plot(epochs, valid_accs, ylabel='Accuracy', name='{}-validation-accuracy-{}'.format(opt.model, config))
+
+
+if __name__ == '__main__':
+    main()
